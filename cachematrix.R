f319d5d453ffae7a0069f5cc243eb40c8c495d5a
Victor H. Cossio <vhca2013@gmail.com>
2015-07-26 11:49:24 -0400

    Programming Assingment 2 solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are part of Coursera's R Programming assingment 2
+## The purpose is to show cashing of the inverse of a matrix
 
-## Write a short comment describing this function
 
+## This function creates an object Matrix that caches it's Inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function 'solve' the inverse of a Matrix. If the inverse has already been 
+## calculated, besides the matrix didn't change, the cachesolve function should 
+## retrieve the cached inverse Matrix (in memory).
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inverso <- x$getinverse()
+    if(!is.null(inverso)) {
+        message("getting cached matrix")
+        return(inverso)
+    }
+    data <- x$get()
+    inverso <- solve(data, ...)
+    x$setinverse(inverso)
+    inverso
 }
